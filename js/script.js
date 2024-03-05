c1e8e7704f3555a87cb8bf6475ce2a90b2b49aa2
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-05 15:24:18 +0100

    password

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23 */
+
+let firstname = prompt("Scrivi il tuo nome");
+console.log(firstname);
+
+let lastname = prompt("Scrivi il tuo cognome");
+console.log(lastname);
+
+let favoriteColor = prompt("Scrivi il tuo colore preferito");
+console.log(favoriteColor);
+
+let randomNumber = 23;
+console.log(randomNumber);
+
+let password = firstname+lastname+favoriteColor+randomNumber;
+
+document.getElementById("titolo").innerHTML = `La tua password è ${password}`;
