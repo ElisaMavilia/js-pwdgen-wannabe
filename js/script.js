2a531160c6adf63d77d19a56473ed24ae2c67111
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-05 15:45:09 +0100

    Add random number

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ console.log(lastName);
 let favoriteColor = prompt("Scrivi il tuo colore preferito");
 console.log(favoriteColor);
 
-let randomNumber = 23;
+let randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
 
 let password = firstName+lastName+favoriteColor+randomNumber;
