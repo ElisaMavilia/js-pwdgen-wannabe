5c8202120d453d74ed74125c7116239cb449dfa5
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-05 15:33:50 +0100

    Adjustments

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito23 */
 
-let firstname = prompt("Scrivi il tuo nome");
-console.log(firstname);
+let firstName = prompt("Scrivi il tuo nome");
+console.log(firstName);
 
-let lastname = prompt("Scrivi il tuo cognome");
-console.log(lastname);
+let lastName = prompt("Scrivi il tuo cognome");
+console.log(lastName);
 
 let favoriteColor = prompt("Scrivi il tuo colore preferito");
 console.log(favoriteColor);
@@ -15,6 +15,6 @@ console.log(favoriteColor);
 let randomNumber = 23;
 console.log(randomNumber);
 
-let password = firstname+lastname+favoriteColor+randomNumber;
+let password = firstName+lastName+favoriteColor+randomNumber;
 
 document.getElementById("titolo").innerHTML = `La tua password è: ${password}`;
